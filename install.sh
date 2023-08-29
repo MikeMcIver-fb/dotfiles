e89bb71d409bf1f2b15d605988204b17dbfb2ec1
Carlton Perkins <carltonperkins@fb.com>
2023-08-29 15:38:13 +0000

    added cargo-binstall

--- install.sh
+++ install.sh
@@ -9,6 +9,12 @@ git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 echo "[INSTALL] Powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+echo "[INSTALL] cargo-binstall"
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+
+echo "[INSTALL] cargo deps"
+cargo -y binstall bat du-dust bandwhich fd-find ripgrep exa git-delta
+
 echo "[INSTALL] Chezmoi"
 export BINDIR=$HOME/.local/bin
 sh -c "$(curl -fsLS chezmoi.io/get)"
